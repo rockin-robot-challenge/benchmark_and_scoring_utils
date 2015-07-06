866f865042e5ab8a682ef284a4f1303badac0fba
Martino Migliavacca <martino.migliavacca@gmail.com>
2015-07-06 17:33:45 +0200

    added fbm1@work environment

--- setup.bash
+++ setup.bash
@@ -1,5 +1,6 @@
 PATH=$PATH:~/workspace/utils
-alias fbm_env='source ~/workspace/utils/fbm_env.sh'
+alias fbmh_env='source ~/workspace/utils/fbmh_env.sh'
+alias fbmw_env='source ~/workspace/utils/fbmw_env.sh'
 alias tbmh_env='source ~/workspace/utils/tbmh_env.sh'
 alias tbmw_env='source ~/workspace/utils/tbmw_env.sh'
 alias fbm_manual='source ~/workspace/utils/fbm_manual_env.sh'
